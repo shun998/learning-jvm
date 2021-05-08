f49bec2a4462cd6ce8d8606258026870081f2674
shun998 <2290860614@qq.com>
2021-05-08 17:12:14 +0800

    add new file.

---
+++ chapter13/src/StringTest1.java
@@ -0,0 +1,15 @@
+/**
+ * @Author: layman
+ * @Date:Create：in 2021/5/8 16:33
+ * @Description:
+ */
+public class StringTest1 {
+    public static void main(String[] args) {
+        System.out.println("打酱油的");
+        try {
+            Thread.sleep(1000000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
